e7eaf382b0c61a704475869745cbc3224053611c
Azevedo vahlis <rafael.azevedo@santosvahlisgroup.com>
2025-06-13 11:40:02 -0300

    fix: build

--- src/components/ButtonContact.tsx
+++ src/components/ButtonContact.tsx
@@ -5,10 +5,10 @@ interface ButtonContactProps {
   fontColor?: string;
 }
 
-export default function ButtonContact<ButtonContactProps>({
+export default function ButtonContact({
   size = "medium",
   fontColor = "#fff",
-}) {
+}: ButtonContactProps) {
   return (
     <button className={`btn-contact btn-${size}`} style={{ color: fontColor }}>
       Fale conosco
